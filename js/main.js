5e6d55d3d00c9711adac806fbd8e838a43657a52
aaron.granger <aaron.granger@my.uwi.edu>
2017-04-19 17:28:01 -0400

    changed look of website

--- js/main.js
+++ js/main.js
@@ -44,3 +44,20 @@ function createTable(records){
     $(sec_id).html(htmlStr);
 } */
 console.log("JavaScript file was successfully loaded in the page");
+
+$(function() {
+    
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+});
